4bb95b3b6a95699561dbec1407729cca7ee37606
ksafranski <ksafranski@gmail.com>
2023-09-03 14:25:03 -0500

    test(graph): add tests for graph class

--- src/graph.ts
+++ src/graph.ts
@@ -1,4 +1,4 @@
-import { GraphNode, type GraphNodeId } from './node'
+import { GraphNode, type GraphNodeId } from './node';
 
 /**
  * Graphs are data structures with nodes (vertexes) and edges. This
@@ -6,65 +6,75 @@ import { GraphNode, type GraphNodeId } from './node'
  * which connect a node to other nodes in the graph
  */
 export class Graph {
-  name: string
+  name: string;
 
-  nodes = new Map<string, GraphNode<any>>()
+  nodes = new Map<string, GraphNode<any>>();
 
   // Defines if edges move in one direction (true) or any (false)
-  directed: boolean = false
+  directed: boolean = false;
 
-  constructor (name: string, directed: boolean = false) {
-    if (!name) throw new Error('Graph must have a name')
-    this.name = name
-    this.directed = directed
+  constructor(name: string, directed: boolean = false) {
+    if (!name) throw new Error('Graph must have a name');
+    this.name = name;
+    this.directed = directed;
   }
 
   // Just check the map for the node and return
-  nodeExists (id: GraphNodeId): GraphNode<any> | undefined {
-    return this.nodes.get(id)
+  nodeExists(id?: GraphNodeId): GraphNode<any> | undefined {
+    if (!id) return undefined;
+    return this.nodes.get(id);
   }
 
   // Creates a new node and adds it to the graph
-  addNode (data: any): GraphNode<any> | undefined {
-    const node = this.nodeExists(data?.id)
-    if (!node || node !== null) {
-      const node = new GraphNode(data)
-      this.nodes.set(node._id, node)
+  addNode<T>(data: GraphNode<T> | any): GraphNode<T | any> {
+    let node = this.nodeExists(data?._id);
+    if (!node || node === null) {
+      node = new GraphNode(data);
+      this.nodes.set(node._id, node);
     }
-    return node
+    return node;
+  }
+
+  // Removes a node from the graph
+  removeNode(id: GraphNodeId): void {
+    const node = this.nodeExists(id);
+    if (node) {
+      this.nodes.delete(id);
+      this.nodes.forEach((n: GraphNode<any>) => n.removeAdjacent(node));
+    }
   }
 
   // Adds an edge by connecting the adjacents between a source node an a destination
-  addEdge (
+  addEdge(
     source: GraphNode<any>,
     destination: GraphNode<any>
   ): Array<GraphNode<any> | undefined> {
-    const sourceNode = this.nodeExists(source._id) || this.addNode(source)
+    const sourceNode = this.nodeExists(source._id) || this.addNode(source);
     const destinationNode =
-      this.nodeExists(destination._id) || this.addNode(destination)
+      this.nodeExists(destination._id) || this.addNode(destination);
     if (sourceNode && destinationNode) {
-      sourceNode.addAdjacent(destinationNode)
+      sourceNode.addAdjacent(destinationNode);
       if (!this.directed) {
-        destinationNode.addAdjacent(sourceNode)
+        destinationNode.addAdjacent(sourceNode);
       }
     }
-    return [sourceNode, destinationNode]
+    return [sourceNode, destinationNode];
   }
 
-  removeEdge (
+  removeEdge(
     source: GraphNode<any>,
     destination: GraphNode<any>
   ): Array<GraphNode<any>> | undefined {
-    const sourceNode = this.nodeExists(source._id)
-    const destinationNode = this.nodeExists(destination._id)
+    const sourceNode = this.nodeExists(source._id);
+    const destinationNode = this.nodeExists(destination._id);
 
     if (sourceNode && destinationNode) {
-      sourceNode.removeAdjacent(destinationNode)
+      sourceNode.removeAdjacent(destinationNode);
       if (!this.directed) {
-        destinationNode.removeAdjacent(sourceNode)
+        destinationNode.removeAdjacent(sourceNode);
       }
 
-      return [sourceNode, destinationNode]
+      return [sourceNode, destinationNode];
     }
   }
 }
--- tests/graph.test.ts
+++ tests/graph.test.ts
@@ -1,13 +1,79 @@
-import { Graph } from '../src/graph'
+import { Graph } from '../src/graph';
+
+let graph: Graph | undefined;
 
 describe('graph', () => {
+  afterEach(() => {
+    graph = undefined;
+  });
   describe('instance', () => {
     it('should be able to be instantiated', () => {
-      const graph = new Graph('test')
-      expect(graph).toBeInstanceOf(Graph)
-    })
+      graph = new Graph('test');
+      expect(graph).toBeInstanceOf(Graph);
+    });
     it('should throw error if no name is provided', () => {
-      expect(() => new Graph('')).toThrowError('Graph must have a name')
-    })
-  })
-})
+      expect(() => new Graph('')).toThrowError('Graph must have a name');
+    });
+  });
+  describe('nodeExists', () => {
+    it('should return undefined if node does not exist', () => {
+      graph = new Graph('test');
+      expect(graph.nodeExists('test')).toBeUndefined();
+    });
+    it('should return the node if it exists', () => {
+      graph = new Graph('test');
+      const node = graph.addNode('test');
+      expect(graph.nodeExists(node?._id)).toBeDefined();
+    });
+  });
+  describe('addNode', () => {
+    it('should add a node to the graph', () => {
+      graph = new Graph('test');
+      const node = graph.addNode('test');
+      expect(graph.nodeExists(node?._id)).toBeDefined();
+    });
+    it('should not add a node if it already exists', () => {
+      graph = new Graph('test');
+      const node = graph.addNode('test');
+      const node2 = graph.addNode(node);
+      expect(node2).toBe(node);
+    });
+  });
+  describe('removeNode', () => {
+    it('should remove a node from the graph', () => {
+      graph = new Graph('test');
+      const node = graph.addNode('test');
+      graph.removeNode(node?._id);
+      expect(graph.nodeExists(node?._id)).toBeUndefined();
+    });
+  });
+  describe('addEdge', () => {
+    it('should add an edge between two nodes', () => {
+      graph = new Graph('test');
+      const node1 = graph.addNode('test');
+      const node2 = graph.addNode('test2');
+      graph.addEdge(node1, node2);
+      expect(node1?.isAdjacent(node2)).toBe(true);
+      expect(node2?.isAdjacent(node1)).toBe(true);
+    });
+    it('should not add an edge if the nodes do not exist', () => {
+      graph = new Graph('test');
+      const node1 = graph.addNode('test');
+      const node2 = graph.addNode('test2');
+      graph.addEdge(node1, node2);
+      expect(node1?.isAdjacent(node2)).toBe(true);
+      expect(node2?.isAdjacent(node1)).toBe(true);
+    });
+  });
+  describe('removeEdge', () => {
+    it('should remove an edge between two nodes', () => {
+      graph = new Graph('test');
+      const node1 = graph.addNode('test');
+      const node2 = graph.addNode('test2');
+      graph.addEdge(node1, node2);
+      graph.removeEdge(node1, node2);
+      expect(node1?.isAdjacent(node2)).toBe(false);
+      expect(node2?.isAdjacent(node1)).toBe(false);
+    });
+  });
+});
