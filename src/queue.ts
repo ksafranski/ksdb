c4b73cfdc7dc63263ef97f9fc81a7f581584571d
ksafranski <ksafranski@gmail.com>
2023-09-03 14:57:02 -0500

    feat(queue): add queue class and tests

--- src/queue.ts
+++ src/queue.ts
@@ -1,19 +1,19 @@
 export class Queue {
-  items = new Set<any>();
+  items = new Set<any>()
 
-  get size(): number {
-    return this.items.size;
+  get size (): number {
+    return this.items.size
   }
 
   push<T>(item: T): Queue {
-    this.items.add(item);
-    return this;
+    this.items.add(item)
+    return this
   }
 
   next<T>(): T | undefined {
-    const arr = [...this.items];
-    const item = arr.shift();
-    this.items = new Set(arr);
-    return item;
+    const arr = [...this.items]
+    const item = arr.shift()
+    this.items = new Set(arr)
+    return item
   }
 }
